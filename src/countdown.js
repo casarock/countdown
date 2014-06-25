0e1c914743ee3506cc7dc4cc7f2684c0963c17a0
Carsten Sandtner <sandtner@mediaman.de>
2014-06-25 15:29:17 +0200

    First working version.

--- src/countdown.js
+++ src/countdown.js
@@ -1,18 +1,99 @@
-(function(){
+(function() {
 
+    function Timer(countTo, renderTime) {
 
+        this.countDown = countTo || 15;
+        this.counter = this.countDown;
+        this.initialCountDown = this.countDown;
+        this.runs = false;
+        this.renderTime = renderTime || false;
+    }
 
-  xtag.register('x-countdown', {
-  	lifecycle: {
-  		created: function(){
+    Timer.prototype.restart = function(newStartValue) {
+        newStartValue = newStartValue || this.initialCountDown;
+        this.start(newStartValue);
+    };
 
-  		}
-  	},
-  	events: {},
-  	accessors: {},
-  	methods: {}
-  });
+    Timer.prototype.stop = function() {
+        this.this.runs = false;
+    };
 
+    Timer.prototype.start = function(startValue) {
 
+        startValue = startValue || this.counter;
+        this.timer = Date.now();
+        this.runs = true;
+        this.countDown = startValue;
+        this._loop.call(this);
+    };
+
+    Timer.prototype.setCountdown = function(countTo) {
+        this.counter = countTo;
+        this.initialCountDown = countTo;
+    };
+
+    Timer.prototype.setTimerElement = function(elementToRender) {
+        this.timerElement = elementToRender;
+    };
+
+    Timer.prototype.getFormatedTime = function(seconds) {
+        var mins = ~~(seconds / 60),
+            secs = seconds % 60;
+
+        return "" + formatNumbers(mins) + ":" + formatNumbers(secs);
+    };
+
+    Timer.prototype._renderTime = function() {
+        this.timerElement.innerHTML = this.getFormatedTime(this.counter);
+    };
+
+    function formatNumbers(value) {
+        return value < 10 ? "0" + value + "" : "" + value;
+    }
+
+    Timer.prototype._updateTimer = function() {
+
+        var newTime = Date.now(),
+            delta = parseInt((newTime - this.timer) / 1000);
+
+        this.counter = this.countDown - delta;
+
+        if (this.renderTime) {
+            this._renderTime.call(this);
+        }
+    };
+
+    Timer.prototype._loop = function() {
+
+        if (this.runs && this.counter > 0) {
+            window.requestAnimationFrame(this._loop.bind(this));
+            this._updateTimer.call(this);
+        } else {
+            this.runs = false;
+            console.log("Stopped");
+        }
+    };
+
+    xtag.register('x-countdown', {
+        lifecycle: {
+            created: function() {
+                this.timer = new Timer(20, false);
+            }
+        },
+        events:    {},
+        accessors: {},
+        methods:   {
+            start: function() {
+                this.timer.start();
+            },
+            stop:  function() {
+                this.timer.stop();
+            },
+            getFormattedTime: function() {
+                return this.timer.getFormatedTime(this.timer.counter);
+            }
+
+        }
+    });
 
 })();
